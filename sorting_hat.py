7e4fd7015a9b1c8e88b3742a60d74ae40dd6ef3d
Will Skywalker <cxbats@gmail.com>
2016-09-27 20:01:48 +0800

    Initial version

---
+++ sorting_hat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+print('戴上分院帽的那一刻，你在想什么？')
+print('A、好担心啊，我究竟会分到哪一个学院呢？')
+print('B、整个学校的人都在看我吗？')
+print('C、这里用开家长会吗？')
+print('D、学院桌上的东西看起来好好吃啊！')
+
+answer = input('回答：')
+
+if answer == 'A':
+    print('格兰芬多!')
+elif answer == 'B':
+    print('斯莱特林!')
+elif answer == 'C':
+    print('赫奇帕奇!')
+elif answer == 'D':
+    print('拉文克劳!')
