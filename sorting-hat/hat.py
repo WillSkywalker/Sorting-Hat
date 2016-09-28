e81f693763ab1c94273e3f9d1945a5a281ebc440
Will Skywalker <cxbats@gmail.com>
2016-09-28 17:48:29 +0800

    Using JSON and adding more questions

--- sorting-hat/hat.py
+++ sorting-hat/hat.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
-total_score = 0
-answer_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
+import json
+import random
+from string import ascii_uppercase
 
-def load_questions(filename):
+total_score = 0
+answer_map = {letter: index for index, letter in enumerate(ascii_uppercase)}
+questions_filename = '../data/questions.json'
+
+def load_questions(filename, number=10):
     with open(filename, encoding='utf-8') as f:
-        q = eval(f.read())
-    return q
+        q = json.load(f)
+    chosen_ones = random.sample(q, number)
+    return chosen_ones
 
 def ask_question(question):
     print(question['question'])
     for a in question['answers']:
         print(a)
     answer = input('回答：')
-    answer_index = answer_map[answer]
+    answer_index = answer_map[answer.upper()]
     return question['scores'][answer_index]
 
-questions = load_questions('questions.txt')
-# 这样做有什么安全隐患吗？你能通过修改 question.txt 来破坏程序的运行吗？
+questions = load_questions(questions_filename)
 
 for question in questions:
     score = ask_question(question)
